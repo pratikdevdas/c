53ad5dfdb716db71709a8be68e404de741b78bb3
Revenant <dasdev.pratik@gmail.com>
2023-02-14 10:04:21 +0530

    ehep

---
+++ helloworld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello,  World!");
+   return 0;
+}
+
+// Find the strength of the linear relationship (Covariance ) of two given data sets (X and Y)
+// if cov(x,y) > 0 then X and Y tend to move in the same direction
+// if cov(x,y) < 0 then X and Y tend to move in the opposite direction
+// if cov(x,y) = 0 then X and Y tend independent
+
